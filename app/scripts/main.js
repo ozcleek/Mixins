d4d0cfd9267db92f89d232bc20a1cb438a45c9e1
Austin Cleek <austincleek@gmail.com>
2014-01-24 15:08:32 -0500

    finished mixins

---
+++ app/scripts/main.js
@@ -0,0 +1,87 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//  Strings
+
+// a word in JavaScript is a string.
+
+// strings are delineatated.
+
+// If you terminate your string in double quotes you can put a lot of stuff in it
+
+// an array: ["fish", "milk", "cheese", "beer", 7]
+
+// anything can be in an array, it can even be empty. You can mix the types.
+
+// arrays can be inside of other arrays.
+
+// a regular expression 
+
+// an object: {}
+
+//  Objective description of the table { legs: 4, height: "5 feet", materials: ['pine', 'iron'], maxCapacity: '400lbs'}
+
+// you can have objects inside of arrays, and arrays inside of objects
+
+// commas used to separate.
+
+// Beware of Trailing Commas! 
+
+// Booleans: true/false statements
+
+// Most thigns in JS valuate true if you test
+
+// function, literal: function () {}
+
+// function type is the only one that can be envoked with parenthesis.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
